d972e3eba472df2cd44a18ffc308629872879c8a
simon <sjtucker28@gmail.com>
2020-03-17 18:13:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+const mapToNegativize = (a) => {
+    let r = [];
+    for (let i = 0; i<a.length; i++) {
+        r.push(-1 * a [i])
+    }
+    return r
+}
+
+const mapToNoChange = a => {
+    let r = [];
+    for (let i = 0; i < a.length; i++) {
+        r.push(a[i])
+    }
+    return r 
+}
+
+const mapToDouble = source => {
+    let r = []
+    for (let i = 0; i < source.length; i++ ) {
+      r.push(2 * source[i])
+    }
+    return r
+}
+
+const mapToSquare = source => {
+    let r = [];
+    for (let i = 0; i < source.length; i++) {
+        r.push(source[i] * source[i])
+    }
+    return r
+}
+
+
+const reduceToTotal = (sourceArray, startingPoint=0) => {
+    let total = startingPoint;
+    for ( let i = 0; i < sourceArray.length; i++) {
+        total = total + sourceArray[i]
+    }
+    return total
+}
+
+const reduceToAllTrue = sourceArray => {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) return false
+    } 
+    return true
+}
+
+const reduceToAnyTrue = sourceArray => {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if(sourceArray[i]) return true 
+    }
+    return false
+}
